1bccf81dc3342f2a9c99418078e045dad5da48fd
Andreas Rau <andreas.rau@mercedes-benz.io>
2020-01-17 21:28:19 +0100

    closes: #21 Refactor observables

--- src/helpers.ts
+++ src/helpers.ts
@@ -12,6 +12,29 @@ export interface Dictionary {
   [property: string]: string | number | undefined;
 }
 
+export const getRandomNumber = (max: number) => {
+  return Math.floor(Math.random() * max);
+};
+
+export const shuffle = ([...arr]) => {
+  let m = arr.length;
+  while (m) {
+    const i = Math.floor(Math.random() * m--);
+    [arr[m], arr[i]] = [arr[i], arr[m]];
+  }
+  return arr;
+};
+
+export const orderBy = (arr, props, orders) =>
+  [...arr].sort((a, b) =>
+    props.reduce((acc, prop, i) => {
+      if (acc === 0) {
+        const [p1, p2] = orders && orders[i] === 'desc' ? [b[prop], a[prop]] : [a[prop], b[prop]];
+        acc = p1 > p2 ? 1 : p1 < p2 ? -1 : 0;
+      }
+      return acc;
+    }, 0)
+  );
 
 /** Returns an array of n random integers in the specified range.
  *
@@ -30,3 +53,10 @@ export const getRandomIntArrayInRange = (min = 0, max = 10, n = 1): number[] =>
  */
 export const zipToDictionary = (props: Array<any>, values: Array<any>): Dictionary =>
   props.reduce((obj, prop, index) => ((obj[prop] = values[index]), obj), {});
+
+export const round = (n, decimals = 0) => {
+  const one = Number(`${n}e${decimals}`)
+  const two = Math.round(one)
+  const three = `${two}e-${decimals}`
+  return Number(three);
+}
